833a60cd646358ecbd0883b97a21e1e658991850
Igor Melo <igorjmelo4@gmail.com>
2019-10-23 23:28:22 -0300

    continuing the project

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '7692c53c8a091d51abc14d5942b37b7e',
+  expiresIn: '7d',
+};
---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'gympoint123',
+  database: 'gympoint',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
